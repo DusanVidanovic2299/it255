2c7000a91a34e11aecdeebc933a60858cf37d142
DusanVidanovic2299 <tennis.fan.atp.wta@gmail.com>
2016-04-22 11:05:21 +0200

    IT255-DZ08
    Dodavanje osmog domaceg zadatka

---
+++ IT255-DZ08-Dusan-Vidanovic-2299/homesig.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Dusan Vidanovic 2299</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="css/custom.css" rel="stylesheet">
+    
+</head>
+
+<body>
+
+    <nav id="siteNav" class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+            </div>
+            <div class="collapse navbar-collapse" id="navbar">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="sign-in">
+                            <a href="index.php">Sign in</a>
+                        </li>
+                    <li class="sign-up">
+                            <a href="register.php">Sign up</a>
+                        </li>
+
+                    <li class="active">
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Rooms</a>
+                    </li>
+					<li>
+						<a href="#">Bookings</a>
+					</li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+                
+            </div>
+        </div>
+    </nav>
+
+    <header>
+        <div class="header-content">
+            <div class="header-content-inner">
+                <h1>Met Hotels</h1>
+                <p>Have a perfect and beautiful vacation with us!</p>
+                <a href="#" class="btn btn-primary btn-lg">BOOK NOW</a>
+            </div>
+        </div>
+    </header>
+
+    <footer class="page-footer">
+    
+        <div class="contact">
+        	<div class="container">
+				<h2 class="section-heading">Contact Us</h2>
+				<p>+381965314749</p>
+				<p><span class="glyphicon glyphicon-envelope"></span><br> methotels@methotels.com</p>
+        	</div>
+        </div>
+        	
+        <div class="small-print">
+        	<div class="container">
+        		<p>Copyright &copy; methotels.com 2016</p>
+        	</div>
+        </div>
+        
+    </footer>
+
+</body>
+
+</html>
